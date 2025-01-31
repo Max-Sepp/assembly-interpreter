94a50db6232e06a0f4383d2dae3d3d11a284d06e
Thomas Zachary <thomaszachary06@proton.me>
2025-01-31 11:22:03 +0000

    replaced simple monadic parser with massively overkill parser combinator

--- app/Main.hs
+++ app/Main.hs
@@ -1,11 +1,11 @@
-{-# LANGUAGE LambdaCase #-}
 module Main where
 
-import Control.Monad.State (get, put, evalStateT, gets, modify, when, unless, execStateT, StateT)
-import Data.Char (isDigit)
+import Data.Char (isSpace)
 import Data.Bits ((.|.), (.&.))
 import Data.Array (Array, bounds, (!), indices, (//), listArray, assocs)
 import System.Environment (getArgs)
+import Control.Applicative (liftA2, Alternative (empty, (<|>)), asum, some, many)
+import Control.Monad.State (StateT, modify, gets, when, unless, execStateT)
 
 type Register = Int
 
@@ -24,71 +24,85 @@ data Instruction
 
 -- Parsing
 
-type Parser a = StateT [String] Maybe a
+newtype P a = P (forall r. String -> (a -> String -> r)
+                                  -> (a -> r)
+                                  -> (String -> r) 
+                                  -> r 
+                                  -> r)
 
-consume :: Parser (Maybe String)
-consume = do
-  xs <- get
-  case xs of
-    []    -> pure Nothing
-    (x:xs') -> put xs' >> pure (Just x)
+runP :: P a -> String -> Maybe a
+runP (P p) str = p str (\x _ -> Just x) Just (const Nothing) Nothing
 
-expectNumber :: Parser Int
-expectNumber = do
-  Just cs@(c:cs') <- consume
-  if all isDigit cs || c == '-' && all isDigit cs'
-    then pure (read cs)
-    else fail "expected digit"
+satisfy :: (Char -> Bool) -> P Char
+satisfy f = P $ \inp cok _ _ eerr ->
+  case inp of
+    c:cs | f c -> cok c cs
+    _          -> eerr
 
-expect2Numbers :: Parser (Int, Int)
-expect2Numbers = (,) <$> expectNumber <*> expectNumber
+atomic :: P a -> P a
+atomic (P x) = P $ \inp cok eok _ eerr ->
+  x inp cok eok (const eerr) eerr
 
-expect3Numbers :: Parser (Int, Int, Int)
-expect3Numbers = (,,) <$> expectNumber <*> expectNumber <*> expectNumber
+instance Functor P where
+  fmap :: (a -> b) -> P a -> P b
+  fmap f (P p) = P $ \inp cok eok cerr eerr ->
+    p inp (cok . f) (eok . f) cerr eerr
 
-parse :: Parser [Instruction]
-parse = do
-  consume >>= \case
-    Just "add" -> do
-      (d, a, b) <- expect3Numbers
-      (Add d a b :) <$> parse
-    Just "sub" -> do
-      (d, a, b) <- expect3Numbers
-      (Sub d a b :) <$> parse
-    Just "mul" -> do
-      (d, a, b) <- expect3Numbers
-      (Mul d a b :) <$> parse
-    Just "shl" -> do
-      (d, a) <- expect2Numbers
-      (Shl d a :) <$> parse
-    Just "shr" -> do
-      (d, a) <- expect2Numbers
-      (Shr d a :) <$> parse
-    Just "bor" -> do
-      (d, a, b) <- expect3Numbers
-      (Bor d a b :) <$> parse
-    Just "band" -> do
-      (d, a, b) <- expect3Numbers
-      (Band d a b :) <$> parse
-    Just "li" -> do
-      (d, i) <- expect2Numbers
-      (Li d i :) <$> parse
-    Just "jz" -> do
-      (d, i) <- expect2Numbers
-      (Jz d i :) <$> parse
-    Just "jp" -> do
-      (d, i) <- expect2Numbers
-      (Jp d i :) <$> parse
-    Nothing -> pure []
-    _ -> fail "invalid command"
+instance Applicative P where
+  pure :: a -> P a
+  pure x = P $ \_ _ eok _ _ -> eok x
 
-getInstructions :: String -> Maybe [Instruction]
-getInstructions code = evalStateT parse (words code)
+  liftA2 :: (a -> b -> c) -> P a -> P b -> P c
+  liftA2 f (P px) (P py) = P $ \inp cok eok cerr eerr ->
+    let cok' x inp' = py inp' (cok . f x) (\y -> cok (f x y) inp') cerr (cerr inp')
+        eok' x      = py inp (cok . f x) (eok . f x) cerr eerr
+    in px inp cok' eok' cerr eerr
+
+instance Alternative P where
+  empty :: P a
+  empty = P $ \_ _ _ _ eerr -> eerr
+
+  (<|>) :: P a -> P a -> P a
+  P px <|> P py = P $ \inp cok eok cerr eerr ->
+    px inp cok eok cerr (py inp cok eok cerr eerr) 
+
+(<:>) :: P Char -> P String -> P String
+px <:> py = (:) <$> px <*> py
+
+char :: Char -> P Char
+char c = satisfy (== c)
+
+space :: P Char
+space = satisfy isSpace
+
+string :: String -> P String
+string = traverse char
+
+oneOf :: [Char] -> P Char
+oneOf = asum . map char
+
+digit :: P Char
+digit = oneOf ['0'..'9']
+
+number :: P Int
+number = read <$> (some digit <|> char '-' <:> some digit)
+
+instruction :: P Instruction
+instruction = asum $ map atomic
+  [ a3 "add" Add, a3 "sub" Sub, a3 "mul" Mul, a3 "bor" Bor, a3 "band" Band
+  , a2 "shl" Shl, a2 "shr" Shr, a2 "li"   Li, a2 "jz"   Jz, a2 "jp"     Jp ]
+  where a3 i c = string i *> space *>
+                  (c <$> number <* space <*> number <* space <*> number)
+        a2 i c = string i *> space *>
+                  (c <$> number <* space <*> number)
+
+instructions :: P [Instruction]
+instructions = many (instruction <* space)
 
 -- Interpreter
 
 data IState = IState
-  { instructions :: Array Int Instruction
+  { program :: Array Int Instruction
   , registers :: Array Int Int
   , pointer :: Int
   , cycleCount :: Int }
@@ -103,16 +117,16 @@ jump p = do
 
 nextInstruction :: Interpreter Instruction
 nextInstruction = do
-  ixs <- gets (indices . instructions)
+  ixs <- gets (indices . program)
   p <- gets pointer
-  is <- gets instructions
+  is <- gets program
   if p `elem` ixs
     then pure (is ! p)
     else fail "invalid pointer position"
 
 isFinished :: Interpreter Bool
 isFinished = do
-  (_, u) <- gets (bounds . instructions)
+  (_, u) <- gets (bounds . program)
   p <- gets pointer
   pure (u == p - 1)
 
@@ -193,8 +207,8 @@ interpret = do
 
 run :: String -> Maybe IState
 run s = do
-  is <- getInstructions s
-  let state = IState { instructions = listArray (0, length is - 1) is
+  is <- runP instructions s
+  let state = IState { program = listArray (0, length is - 1) is
                      , registers    = listArray (0, 15) (replicate 16 0)
                      , pointer      = 0
                      , cycleCount   = 0 }
@@ -204,7 +218,7 @@ run s = do
 main :: IO ()
 main = do
   (a:_) <- getArgs
-  ms <- run <$> readFile a
+  ms <- run . (++" ") <$> readFile a
   case ms of
     Nothing -> print "interpretation failed"
     Just s  -> do
