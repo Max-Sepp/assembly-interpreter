7b1ce702894b21c76e35ba2ea4fb7cca2e065948
Max Smith <103558620+Max-Sepp@users.noreply.github.com>
2025-02-07 11:29:30 +0000

    (feat) added table register print

--- app/Main.hs
+++ app/Main.hs
@@ -2,13 +2,13 @@ module Main where
 
 import Data.Char (isSpace)
 import Data.Bits ((.|.), (.&.), shiftL, shiftR, )
-import Data.Array (Array, bounds, (!), indices, (//), listArray, assocs)
+import Data.Array (Array, bounds, (!), indices, (//), listArray, elems)
 import System.Environment (getArgs)
 import Control.Applicative (liftA2, Alternative (empty, (<|>)), asum, some, many)
 import Control.Monad.State (StateT, modify, gets, when, unless, execStateT, lift)
 import Data.Functor (($>))
 import Data.Int (Int32)
-import Data.List (dropWhileEnd)
+import Data.List (dropWhileEnd, intercalate)
 import Data.List.Split (splitOn)
 
 type Register = Int32
@@ -30,8 +30,8 @@ data Instruction
 
 newtype P a = P (forall r. String -> (a -> String -> r)
                                   -> (a -> r)
-                                  -> (String -> r) 
-                                  -> r 
+                                  -> (String -> r)
+                                  -> r
                                   -> r)
 
 runP :: P a -> String -> Either String a
@@ -72,7 +72,7 @@ instance Alternative P where
 
   (<|>) :: P a -> P a -> P a
   P px <|> P py = P $ \inp cok eok cerr eerr ->
-    px inp cok eok cerr (py inp cok eok cerr eerr) 
+    px inp cok eok cerr (py inp cok eok cerr eerr)
 
 eof :: P ()
 eof = P $ \inp _ eok _ eerr ->
@@ -84,7 +84,7 @@ eof = P $ \inp _ eok _ eerr ->
 px <:> py = (:) <$> px <*> py
 
 (<|+>) :: P [a] -> P a -> P [a]
-px <|+> py = ((++) <$> px <*> ((: []) <$> py)) <|> px
+px <|+> py = (++) <$> px <*> ((: []) <$> py) <|> px
 
 char :: Char -> P Char
 char c = satisfy (== c)
@@ -95,7 +95,7 @@ space = some (satisfy isSpace) $> ()
 string :: String -> P String
 string = traverse char
 
-oneOf :: [Char] -> P Char
+oneOf :: String -> P Char
 oneOf = asum . map char
 
 digit :: P Char
@@ -133,8 +133,7 @@ increment :: Interpreter ()
 increment = modify (\s -> s { pointer = pointer s + 1 })
 
 jump :: Int32 -> Interpreter ()
-jump p = do
-  modify (\s -> s { pointer = p })
+jump p = modify (\s -> s { pointer = p })
 
 nextInstruction :: Interpreter Instruction
 nextInstruction = do
@@ -170,15 +169,15 @@ countCycles n = modify (\s -> s { cycleCount = cycleCount s + n })
 
 safeAdd :: Int32 -> Int32 -> Interpreter Int32
 safeAdd x y
-  | (y > 0 && s < x) || (y < 0 && s > x) = ierror "Integer overflow occurred"
+  | y > 0 && s < x || y < 0 && s > x = ierror "Integer overflow occurred"
   | otherwise = pure s
   where s = x + y
 
-safeMul :: Int32 -> Int32 -> Interpreter Int32 
+safeMul :: Int32 -> Int32 -> Interpreter Int32
 safeMul x y
-  | (res `div` y) /= x = ierror "Integer overflow occurred"
+  | res `div` y /= x = ierror "Integer overflow occurred"
   | otherwise = pure res
-  where 
+  where
     res = x * y
 
 runInstruction :: Instruction -> Interpreter ()
@@ -251,7 +250,7 @@ numberRegisters = 16
 
 getInitialRegisters :: [String] -> [Int32]
 getInitialRegisters []       = replicate numberRegisters 0
-getInitialRegisters (rstr:_) = rs ++ replicate (numberRegisters - length rs) 0 
+getInitialRegisters (rstr:_) = rs ++ replicate (numberRegisters - length rs) 0
   where
     rs = toInt32List (splitOn "," rstr)
 
@@ -262,6 +261,40 @@ initialState rs is =
          , pointer      = 0
          , cycleCount   = 0 }
 
+showArrayTable :: (Num a, Enum a, Show a, Show b, Integral a) => 
+                  Array a b -> String
+showArrayTable rs =
+    row ++ "\n"
+    ++ indexRow ++ "\n"
+    ++ row ++ "\n"
+    ++ registerRow ++ "\n"
+    ++ row ++ "\n"
+    where
+        (minIndex, maxIndex) = bounds rs
+
+        elementWidth =
+            maximum (
+                map (length . show) [minIndex..maxIndex]
+                ++ map (length . show) (elems rs)
+            )
+
+        numElements = fromIntegral (maxIndex - minIndex + 1)
+
+        row = "+-" ++
+              intercalate "-+-"
+                (replicate numElements (replicate elementWidth '-'))
+              ++ "-+"
+
+        pad :: String -> String
+        pad cs = replicate (elementWidth - length cs) ' ' ++ cs
+
+        indexStringList = map (pad . show) [minIndex..maxIndex]
+
+        indexRow = "| " ++ intercalate " | " indexStringList ++ " |"
+
+        registerStringList = map (pad . show) (elems rs)
+
+        registerRow = "| " ++ intercalate " | " registerStringList ++ " |"
 
 main :: IO ()
 main = do
@@ -269,15 +302,15 @@ main = do
   c <- dropWhileEnd isSpace <$> readFile a
 
   is <- case runP instructions c of
-    Right is -> pure is 
+    Right is -> pure is
     Left x -> ioError $ userError ("parse error at: \"" ++ x ++ "\"")
 
   let initialRegisters = getInitialRegisters rest
-  
+
   st <- case execStateT interpret (initialState initialRegisters is) of
     Right st -> pure st
     Left x -> ioError $ userError ("interpretation error at: \"" ++ x ++ "\"")
 
   putStrLn $ "cycles: " ++ show (cycleCount st)
   putStrLn "registers:"
-  print (assocs (registers st))
+  putStr $ showArrayTable (registers st)
